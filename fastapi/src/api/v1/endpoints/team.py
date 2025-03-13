423ecb18a5c9feb360cc78e3cefb540b08e60fd8
Gabriel Botareli <gbotareli@gmail.com>
2025-03-13 16:11:38 -0300

    feat: Add team management endpoints for retrieving and adding team members

---
+++ fastapi/src/api/v1/endpoints/team.py
@@ -0,0 +1,44 @@
+from typing import Optional
+
+from db.database import get_session
+from db.models import Team_Members, Users
+from pydantic import BaseModel
+from sqlalchemy.orm import Session
+from utils.security import get_current_user, is_user_authenticated
+
+from fastapi import APIRouter, Depends, HTTPException, status
+
+
+class Team(BaseModel):
+    member: list[Team_Members]
+
+
+router = APIRouter(prefix="/team")
+
+
+@router.get("/me", response_model=Team)
+def get_my_team(current_user: Users = Depends(get_current_user)) -> Optional[Team]:
+    is_user_authenticated(current_user)
+
+    team_members = current_user.team_members
+    if not team_members:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="No team members found"
+        )
+
+    return Team(member=team_members)
+
+
+@router.post("/add", response_model=Team)
+def add_team_member(
+    member: Team_Members,
+    db: Session = Depends(get_session),
+    current_user: Users = Depends(get_current_user),
+) -> Optional[Team]:
+    is_user_authenticated(current_user)
+
+    db.add(member)
+    db.commit()
+    db.refresh(member)
+
+    return Team(member=[member])
